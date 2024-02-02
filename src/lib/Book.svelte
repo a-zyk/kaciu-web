5f83ce8cef00ccd21bccff20ecdc5d00cb3293ac
a-zyk <austejazykute@gmail.com>
2024-02-02 12:30:29 +0200

    change book text

--- src/lib/Book.svelte
+++ src/lib/Book.svelte
@@ -1,16 +1,22 @@
 <div
   id="apie"
-  class="container  flex flex-col items-center justify-center md:flex-row gap-8 py-8 md:py-12"
+  class="container flex flex-col items-center justify-center md:flex-row gap-8 py-8 md:py-12"
 >
   <div class="md:max-w-[400px] flex flex-col self-start items-center">
-    <h2 class="mb-4 text-4xl tracking-tight text-center  text-gray-900">
+    <h2 class="mb-4 text-4xl tracking-tight text-center text-gray-900">
       Katės šeimininko elementorius
     </h2>
     <p class="mb-5 font-light text-gray-500 sm:text-xl">
-      Katės šeimininko elementorius padės suprasti kačių elgseną ir padės
-      sukurti artimesnį ryšį su augintiniu. Knygą galite įsigyti popieriniu, PDF
-      ar audio formatu. Dėl įsigyjimo kreipkitės <a
-        href="mailto:kaciuelgesysa@gmail.com"
+      Elementorius- pagrindinė ir fundamentinė žinių bazė, kurias privalėtų
+      žinoti kiekvienas katės šeimininkas. Jame rasite 16 praktinių patarimų
+      apie gyvenimą su kate. Taikydami šias taisykles suprasite kačių poreikius,
+      galėsite geriau pasirūpinti jų sveikata. Tai leis sukurti geresnį ryšį su
+      augintiniu ir užtikrins, kad laikas praleistas kartu bus malonus ir
+      mielas.
+    </p>
+    <p class="mb-5 font-light text-gray-500 sm:text-xl">
+      Knygą galite įsigyti popieriniu, PDF ar audio formatu. Dėl įsigyjimo
+      kreipkitės <a href="mailto:kaciuelgesysa@gmail.com"
         >kaciuelgesys@gmail.com</a
       >
     </p>
