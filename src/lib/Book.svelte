6c73cf1ac9d84e326f10828b407d30038450decb
a-zyk <austejazykute@gmail.com>
2024-02-02 12:20:22 +0200

    add rounded corners to book img

--- src/lib/Book.svelte
+++ src/lib/Book.svelte
@@ -1,6 +1,6 @@
 <div
   id="apie"
-  class="container flex flex-col items-center justify-center md:flex-row gap-8 py-8 md:py-12"
+  class="container  flex flex-col items-center justify-center md:flex-row gap-8 py-8 md:py-12"
 >
   <div class="md:max-w-[400px] flex flex-col self-start items-center">
     <h2 class="mb-4 text-4xl tracking-tight text-center  text-gray-900">
@@ -15,7 +15,7 @@
       >
     </p>
   </div>
-  <div class="md:max-w-[400px]  rounded-lg overflow-hidden">
+  <div class="md:max-w-[400px] rounded-3xl overflow-hidden">
     <img class="w-full h-auto" src="/book.jpg" alt="book cover" />
   </div>
 </div>
